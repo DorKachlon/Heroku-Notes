00413ef7543306b9ff0d3647fed2caa984277922
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-08-31 18:12:57 +0300

    heroku 2

---
+++ build/precache-manifest.b9e07a366120e04a7e4fb8e3a51ee9b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e948addcdad0acb8b4c19a80126502a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb9b7049283ef7e2882",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccb9b7049283ef7e2882",
+    "url": "/static/js/main.ec8b76c6.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.static("build"));
 
 let notes = [
     {
